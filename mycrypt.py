bfc3713f16dcea24eea73dc4f06998ce191d558e
Svarkku <80623992+Svarkku@users.noreply.github.com>
2021-03-14 20:49:14 +0200

    Add files via upload
    labra2

---
+++ mycrypt.py
@@ -0,0 +1,31 @@
+import codecs
+
+def encode(s):
+    if not isinstance(s,str):
+        raise TypeError
+    origlen = len(s)
+    crypted = ""
+    digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
+    if len(s) > 1000:
+        raise ValueError
+    for c in s:
+        if c == "å" or c == "ä" or c == "ö":
+            raise ValueError
+        elif c.isalpha():
+            if c.islower():
+                c=c.upper()
+            # Rot13 the character for maximum security
+            crypted+=codecs.encode(c,'rot13')
+        elif c in digitmapping:
+          crypted+=digitmapping[c]
+        else:
+            raise ValueError
+
+    return crypted
+
+def decode(s):
+    d = encode(s)
+    if d.isalpha():
+        d =d.lower()
+    return d
+    
